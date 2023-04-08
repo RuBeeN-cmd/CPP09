e2f6d4f7230ba4a005278cc028c56f1447b38c4e
Ruben Rollin <rrollin@2E4.42angouleme.fr>
2023-04-08 12:49:19 +0200

    push

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -32,21 +32,40 @@ void	print_list(std::list<int> &lst) {
 	std::cout << std::endl;
 }
 
-void	ft_insert_sort(std::list<int> &lst) {
-	std::list<int>::iterator it = lst.begin();
-	while (it != lst.end())
-    	std::rotate(std::upper_bound(lst.begin(), it, *it), it++, it);
+void	ft_insert_sort(std::list<int>::iterator begin, std::list<int>::iterator end) {
+	std::list<int>::iterator it = begin;
+	std::list<int>::iterator j = it;
+	j++;
+	while (it != end) {
+		std::rotate(std::upper_bound(begin, it, *it), it, j);
+		it++;
+		j++;
+	}
 }
 
-void	ft_merge_insert_sort(std::list<int> &lst, unsigned int k = 4) {
-	if (lst.size() < k)
-		ft_insert_sort(lst);
+void	ft_merge_insert_sort(std::list<int>::iterator begin, std::list<int>::iterator end, unsigned int k = 4) {
+	
+	std::list<int>::iterator sub_end = begin;
+	for (unsigned int i = 0; i < *end / k; i++)
+	{
+		size_t j = 0;
+		while (j < k && *sub_end != *end)
+		{
+			sub_end++;
+			j++;
+		}
+		ft_insert_sort(begin, sub_end);
+		begin = sub_end;
+	}
 }
+
 
 int main(int argc, char *argv[]) {
 	std::list<int> lst = get_list(argc, argv);
 	print_list(lst);
-	ft_merge_insert_sort(lst);
+	ft_merge_insert_sort(lst.begin(), lst.end());
+
+
 	print_list(lst);
 	
 	return (0);
